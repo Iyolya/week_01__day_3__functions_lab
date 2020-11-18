b00a37ad130e57bd060438ef3ebc9ebdc7521396
Laura <almasilaura@outlook.com>
2020-11-18 13:50:37 +0000

    commited first line of test.py

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -21,6 +21,64 @@ def multiply(number1, number2):
 def divide(number1, number2):
       return number1 / number2
 
+# 6.
+
+  def length_of_string(string):
+      return len(string = "A string of length 21")
+      
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
 # 2.
 # 2.
 # 2.
