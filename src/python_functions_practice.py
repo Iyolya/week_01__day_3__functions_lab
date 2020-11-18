e3a2eac97b9276bf32f61cbce03b4df088bb2a0b
Laura <almasilaura@outlook.com>
2020-11-18 13:28:19 +0000

    We just added missing files and the first three functions

---
+++ src/python_functions_practice.py
@@ -0,0 +1,22 @@
+#1.
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def test_subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+
+# 2.
+# 2.
+# 2.
+      
+
+
