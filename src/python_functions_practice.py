492da3124a48c595ee96908da053aca84d3158ba
Laura <almasilaura@outlook.com>
2020-11-18 14:47:16 +0000

    Added function 8. and 9.

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,4 +1,4 @@
-#1.
+# 1.
 
 def return_10():
       return 10 
@@ -26,6 +26,8 @@ def divide(number1, number2):
 def length_of_string(string):
       string = "A string of length 21"
       return len(string)
+      #def length_of_string(string_1)
+            #return len(string_1)
       
 # 7.
 
@@ -34,5 +36,32 @@ def join_string(string1, string2):
       string2 = "its fleece was white as snow"
       return string1 + string2
       
+# 8.
+
+def test_add_string_as_number(number1, number2):
+      return int(number1) + int(number2)
+
+
+# 9.
+
+# def number_to_full_name__month(month_number):
+#       if month_number == 1:
+#             return "January"
+#       elif month_number == 3:
+#             return "March"
+#       elif month_number == 4:
+#             return "April"
+#       elif month_number == 9:
+#             return "September"
+#       elif month_number == 10:
+#             return "October"      
+
+
+# 10.
+
+# def number_to_full_name__month(month_number):
+#       full_month_name = number_to_full_name__month(month_number)
+#       short_month_name = full_month_name[0:3] #[0:3] means we will got 3 characters from position 0 ?
+#       return short_month_name
 
 
