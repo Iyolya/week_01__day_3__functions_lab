993e1e6d36dd4cdc82c5a134970d3ef24b3f123f
Laura <almasilaura@outlook.com>
2020-11-18 13:58:47 +0000

    Function 5 added

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -23,9 +23,253 @@ def divide(number1, number2):
 
 # 6.
 
-  def length_of_string(string):
-      return len(string = "A string of length 21")
+def length_of_string(string):
+      string = "A string of length 21"
+      return len(string)
       
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
+# 2.
+# 2.
+      
+
+
+def return_10():
+      return 10 
+# 2.
+
+def add(number1, number2):
+      return number1 + number2
+# 3.
+
+def subtract(number1, number2):
+      return number1 - number2
+
+# 4.
+
+def multiply(number1, number2):
+      return number1 * number2      
+
+# 5.
+
+def divide(number1, number2):
+      return number1 / number2
+
+# 2.
 # 2.
 # 2.
       
